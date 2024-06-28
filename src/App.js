0e8310919d1cb212d699d1647576598b0c6519c0
limshiun <shiun41@gmail.com>
2024-06-29 00:23:22 +0800

    Added foundation

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import ContactCard  from './ContactCard';
+
+
+const App =()=>{
+      const [results,setResults] = useState([]);
+
+      useEffect (()=>{
+        fetch("https://randomuser.me/api/?results=10")
+          .then (response => response.json())
+          .then (data => {
+          console.log(data)
+          setResults(data.results)
+      });
+    }, [])
+
+  return (
+    <div>
+      {results.map((result,index)=>{
+        return(
+          <ContactCard 
+          key={index}
+          avatarUrl= {result.picture.large}
+          name = {result.name.first}
+          email = {result.email}
+          age = {result.dob.age}/>
+        )
+      })}
+    </div>
+  )
+}
+
+export default App;
